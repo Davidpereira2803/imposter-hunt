b4b28b8957fa9760889479fe284028e1bb9b5d68
Davidpereira2803 <david.pereira.002@student.uni.lu>
2025-11-28 18:27:12 +0100

    refactor: add platform-specific ad unit ID logic for better ad management

--- src/components/AdBanner.js
+++ src/components/AdBanner.js
@@ -1,9 +1,17 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Platform } from "react-native";
 import Constants from "expo-constants";
 import { BannerAd, BannerAdSize, TestIds } from "react-native-google-mobile-ads";
 import { useAdConsentContext } from "../contexts/AdConsentContext";
 import ADS_CONFIG from "../config/adshelper";
+
+
+{/*
+const prodUnitId = 
+  Platform.OS === "ios"
+    ? ADS_CONFIG.IOS_BANNER_UNIT_ID
+    : ADS_CONFIG.ANDROID_BANNER_UNIT_ID;
+*/}
 
 const adUnitId = __DEV__
   ? TestIds.BANNER
